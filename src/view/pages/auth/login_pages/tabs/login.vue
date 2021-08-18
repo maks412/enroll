5f903497c8db5a812b9ce7779bbc4a244352997a
maks412 <galiev.maksat@gmail.com>
2021-08-18 11:11:50 +0600

    forgot

--- src/view/pages/auth/login_pages/tabs/login.vue
+++ src/view/pages/auth/login_pages/tabs/login.vue
@@ -19,7 +19,8 @@
     <div class="form-group">
       <div class="d-flex justify-content-between mt-n5">
         <label class="font-size-h6 font-weight-bolder text-dark pt-5">Password</label>
-        <a class="text-primary font-size-h6 font-weight-bolder text-hover-primary pt-5 clickable" @click="$emit('showForm','forgot')">Forgot Password ?</a>
+        <!-- FORGOT DELETED!! -->
+        <!-- <a class="text-primary font-size-h6 font-weight-bolder text-hover-primary pt-5 clickable" @click="$emit('showForm','forgot')">Forgot Password ?</a> -->
       </div>
       <div label-for="example-input-2">
         <input
